998aaa2ab33449a7888dadd008d5993c34472278
devmatheus <matheustakeuchi1@gmail.com>
2023-06-13 23:00:29 -0300

    up cloud

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Header: React.FC = () => {
+  return <Container>Interface reproduzida com a DevMatheus</Container>;
+};
+
+export default Header;
